de709e1f52e4aed24eff84253cf81ccf0cec25db
brunosoaresgamedev <80013167+brunosoaresgamedev@users.noreply.github.com>
2021-06-18 17:10:06 -0300

    2
    2

---
+++ Bruno2DActionPlatformGame/Assets/Game/Scripts/Enemies/EnemieAIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer2D.Character;
+[RequireComponent(typeof(CharacterFacing2D))]
+[RequireComponent(typeof(CharacterMovement2D))]
+public class EnemieAIController : MonoBehaviour
+{
+    CharacterFacing2D enemyFacing;
+    CharacterMovement2D enemyMovement;
+    public Vector2 movementInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyFacing = GetComponent<CharacterFacing2D>();
+        enemyMovement = GetComponent<CharacterMovement2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyMovement.ProcessMovementInput(movementInput);
+        enemyFacing.UpdateFacing(movementInput);
+    }
+
+}
---
+++ Bruno2DActionPlatformGame/Assets/Game/Scripts/Objects/FadeTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTree : MonoBehaviour
+{
+
+    public bool EnterCollider;
+    private Renderer rend;
+
+
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnterCollider = false;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnterCollider == true)
+        {
+            rend.material.color = colorToTurnTo;
+        }
+        if(EnterCollider == false)
+        {
+            rend.material.color = Color.white;
+        }
+    }
+        
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EnterCollider = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EnterCollider = false;
+        }
+    }
+   
+
+}
