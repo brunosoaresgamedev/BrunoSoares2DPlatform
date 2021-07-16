72239ad64929eff78e2a775e565fec8f93ba9f7d
brunosoaresgamedev <brunosoaresgamedev@gmail.com>
2021-07-16 15:24:21 -0300

    Start menu done
    Menu and option in game done

---
+++ Bruno2DActionPlatOld/Assets/Game/Scripts/UI/Menu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    float GetVol(float vol)
+    {
+        float newVol = 0;
+        newVol = 20 * Mathf.Log10(vol);
+        if (vol <= 0)
+        {
+            newVol = -80;
+        }
+
+        return newVol;
+    }
+
+
+    public void SetMasterVol(float vol)
+    {
+        
+        mixer.SetFloat("Master", GetVol(vol));
+
+    }
+
+
+    public void SetMusicVol(float vol)
+    {
+        mixer.SetFloat("Music", GetVol(vol));
+    }
+    
+
+    public void Loading()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
